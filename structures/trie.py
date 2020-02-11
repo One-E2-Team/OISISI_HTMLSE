793b9507b1504344b10d07a3cfdaebc4d32c4f6a
Radoš Milićev <rados280698@yahoo.com>
2020-02-11 12:53:39 +0100

    My first touch with python and initial #trie implementation

--- structures/trie.py
+++ structures/trie.py
@@ -1,4 +1,49 @@
+class TrieNode:
+    def __init__(self, letter):
+        self.letter = letter
+        self.is_end = False
+        self.children = {}
+
 class Trie:
     # TODO implement Trie
     def __init__(self):
-        pass
+        self.root = TrieNode("*")
+
+    def word_exists(self, word):
+        if word == "":
+            return True
+        current_node = self.root
+        for letter in word:
+            if letter not in current_node.children:
+                return False
+            current_node = current_node.children[letter]
+
+        if current_node.is_end:
+            return True
+        else:
+            return False
+
+
+    def add_node(self, word):
+        current_node = self.root
+        for letter in word:
+            if letter not in current_node.children:
+                adding = TrieNode(letter)
+                current_node.children[letter] = adding
+            current_node = current_node.children[letter]
+        current_node.is_end = True
+
+    def __str__(self):
+        if self.root.children:
+            return "Trie is not empty"
+        else:
+            return "Trie is empty"
+
+if __name__ == "__main__":
+    trie = Trie()
+    trie.add_node("slovo")
+    trie.add_node("slova")
+    trie.add_node("word")
+    trie.add_node("words")
+    trie.add_node("slo")
+    print(trie)
