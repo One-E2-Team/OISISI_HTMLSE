5f06b2f6243b1706fbea9b4c7b30d2fe9ca9036a
Radoš Milićev <rados280698@yahoo.com>
2020-02-18 16:35:22 +0100

    #parser(stablo parsiranja) blossoms like done

--- search/advanced/query_parser.py
+++ search/advanced/query_parser.py
@@ -3,6 +3,15 @@ import search
 from parglare import Parser, Grammar
 
 
+class Node:
+    def __init__(self, value, children=[]):
+        self.value = value
+        self.children = children
+
+    def __str__(self):
+        return str(self.value)
+
+
 def parse(input_query: str):
     input_query = search.get_correct_query(input_query)
     grammar = r"""
@@ -19,15 +28,15 @@ def parse(input_query: str):
     string: /[^&!|()]+/;
     """
     actions = {
-        "E": [lambda _, n: n[0] + '||' + n[2],
-              lambda _, n: n[0] + '||' + n[1],
-              lambda _, n: n[0] + '&&' + n[2],
-              lambda _, n: '!' + n[1],
-              lambda _, n: '(' + n[1] + ')',
-              lambda _, n: n[0]],
-        "EE": [lambda _, n: n[0],
-               lambda _, n: '(' + n[1] + ')'],
-        "string": lambda _, value: str(value).lower(),
+        "E": [lambda _, n: Node('||', [n[0], n[2]]),
+              lambda _, n: Node('||', [n[0], n[1]]),
+              lambda _, n: Node('&&', [n[0], n[2]]),
+              lambda _, n: Node('!', [n[1]]),
+              lambda _, n: Node('()', [n[1]]),
+              lambda _, n: Node(n[0])],
+        "EE": [lambda _, n: Node(n[0]),
+               lambda _, n: Node('()', [n[1]])],
+        "string": lambda _, value: Node(value.lower()),
     }
     g = Grammar.from_string(grammar)
     parser = Parser(g, actions=actions)
