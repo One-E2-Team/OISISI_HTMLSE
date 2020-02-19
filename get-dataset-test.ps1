f4371faf3c1a71c4538feddd95415fb6e5a0faa5
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-02-19 17:36:43 +0100

    script update

--- get-dataset-test.ps1
+++ get-dataset-test.ps1
@@ -1,6 +1,6 @@
 Remove-Item -Force -Confirm:$false .\test-skup.zip
 Remove-Item -Recurse -Force -Confirm:$false .\test-skup
-$url = "https://fmasterofu.github.io/OISISI_Python/dataset/test-skup.zip"
+$url = "https://fmasterofu.github.io/OISISI_HTMLSE/dataset/test-skup.zip"
 $output = "$PSScriptRoot\test-skup.zip"
 Invoke-WebRequest -Uri $url -OutFile $output
 Expand-Archive $output
