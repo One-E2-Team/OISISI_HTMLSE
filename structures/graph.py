f9562ffa267df2cf6b6cf2b62e10aa3be7461653
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-02-21 20:52:39 +0100

    comments and time complexities for structures, student 2

--- structures/graph.py
+++ structures/graph.py
@@ -6,9 +6,18 @@ class Graph:
             self._element = x
 
         def endpoints(self):
+            """
+            O(1)
+            :return: tuple with vertices - endpoints
+            """
             return self._origin, self._destination
 
         def opposite(self, v):
+            """
+            returns opposite vertex on edge, O(1)
+            :param v: given vertex
+            :return: opposite vertex
+            """
             if v == self._origin or v == self._destination:
                 return self._destination if v == self._origin else self._origin
             else:
@@ -33,10 +42,19 @@ class Graph:
         self._incoming = {} if directed else self._outgoing
 
     def _validate_vertex(self, v):
+        """
+        Checks if a given vertex is in graph (otherwise raises ValueError), in O(1), abs worst case is O(n).
+        :param v: given vertex
+        :return: None
+        """
         if v not in self._outgoing:
             raise ValueError('Vertex does not belong to this graph.')
 
     def is_directed(self):
+        """
+        O(1)
+        :return:
+        """
         return self._incoming is not self._outgoing
 
     def vertex_count(self):
@@ -50,28 +68,55 @@ class Graph:
         return total if self.is_directed() else total // 2
 
     def edges(self):
+        """
+        Never used, so don't mind the forbidden apple in form of set()
+        :return:
+        """
         result = set()  # avoid double-reporting edges of undirected graph
         for secondary_map in self._outgoing.values():
             result.update(secondary_map.values())  # add edges to resulting set
         return result
 
     def get_edge(self, u, v):
+        """
+        O(1) brought to you by the power of hash map
+        :param u: vertex 1
+        :param v: vertex 2
+        :return: edge if vertices are adjacent, otherwise None
+        """
         self._validate_vertex(u)
         self._validate_vertex(v)
         return self._outgoing[u].get(v)  # returns None if v not adjacent
 
     def degree(self, v, outgoing=True):
+        """
+        returns degree of a vertex in graph, O(1)
+        :param v: vertex
+        :param outgoing: graph type
+        :return: degree (integer)
+        """
         self._validate_vertex(v)
         adj = self._outgoing if outgoing else self._incoming
         return len(adj[v])
 
     def incident_edges(self, v, outgoing=True):
+        """
+        returns a generator object of incident edges for a vertex O(m), m connected vertices
+        :param v: vertex
+        :param outgoing: type
+        :return: generator object
+        """
         self._validate_vertex(v)
         adj = self._outgoing if outgoing else self._incoming
         for edge in adj[v].values():
             yield edge
 
     def insert_vertex(self, v):
+        """
+        Inserts lone vertex, exception on already existing vertex, in O(1)
+        :param v: vertex
+        :return: vertex
+        """
         try:
             self._validate_vertex(v)
         except ValueError:
@@ -81,6 +126,13 @@ class Graph:
             return v
 
     def insert_edge(self, u, v, x=None):
+        """
+        Inserts edge in O(1)
+        :param u: vertex 1
+        :param v: vertex 2
+        :param x: edge weight
+        :return: None (can raise exceptions)
+        """
         if self.get_edge(u, v) is not None:  # includes error checking
             raise ValueError('u and v are already adjacent')
         e = self.Edge(u, v, x)
@@ -88,6 +140,11 @@ class Graph:
         self._incoming[v][u] = e
 
     def remove_vertex(self, v):
+        """
+        Removes all edges on given vertex and the vertex itself, in O(m), m connected edges
+        :param v: vertex
+        :return: None
+        """
         self._validate_vertex(v)
         for e in list(self._outgoing[v].keys()):
             self.remove_edge(Graph.Edge(v, e, None))
@@ -98,6 +155,11 @@ class Graph:
         del self._outgoing[v]
 
     def remove_edge(self, e):
+        """
+        Removes given edge in O(1)
+        :param e: edge
+        :return: None (method can raise exceptions)
+        """
         if not isinstance(e, Graph.Edge):
             raise TypeError('Edge expected')
         u, v = e.endpoints()
@@ -107,6 +169,12 @@ class Graph:
         del self._incoming[v][u]
 
     def remove_edge_uv(self, u, v):
+        """
+        removes edge by given vertices, in O(1)
+        :param u: vertex 1
+        :param v: vertex 2
+        :return: None (method can raise exceptions)
+        """
         self.remove_edge(Graph.Edge(u, v, None))
 
     def __str__(self):
--- structures/set.py
+++ structures/set.py
@@ -1,8 +1,17 @@
 class Set:
+    """
+    Using internal python set() was forbidden. Many optimization would be possible otherwise (especially in &, |, -
+    and get_list methods)
+    """
     def __init__(self):
         self._dict = {}
 
     def add(self, e):
+        """
+        Method adds element to SET in O(1)
+        :param e: element to be added to set
+        :return: None
+        """
         self._dict[e] = e
 
     def discard(self, e):
@@ -12,9 +21,17 @@ class Set:
             return
 
     def __contains__(self, e):
+        """
+        Method returns boolean type indicating if a specific object is present in Set, in O(1), abs worst case is O(n).
+        :param e: tested element
+        :return: True/False
+        """
         return e in self._dict
 
     def __len__(self):
+        """"
+        Length O(1)
+        """
         return len(self._dict)
 
     def __iter__(self):
@@ -34,12 +51,22 @@ class Set:
         return self._dict.copy()
 
     def __or__(self, other):  # union
+        """
+        Performs union of two sets in O(n) where n is the sum of number of elements of both sets.
+        :param other: second set
+        :return: resulting Set (union)
+        """
         ret = Set()
         for e in list(self._dict.keys()) + list(other.__get_dict().keys()):
             ret.add(e)
         return ret
 
     def __and__(self, other):  # cross section
+        """
+        Performs cross-section of two Sets in O(n).
+        :param other:  second Set
+        :return: resulting Set (cross-section)
+        """
         ret = Set()
         for e in list(self._dict.keys()) + list(other.__get_dict().keys()):
             if e in self._dict and e in other:
@@ -47,6 +74,11 @@ class Set:
         return ret
 
     def __sub__(self, other):  # difference/complement
+        """
+        Performs difference (complement) of two Sets in O(n).
+        :param other: second Set
+        :return: resulting Set (complement)
+        """
         ret = Set()
         for e in self._dict:
             if e not in other.__get_dict():
@@ -57,4 +89,8 @@ class Set:
         return str(list(self._dict.keys()))
 
     def get_list(self):
+        """
+        O(n)
+        :return: List of Set elements.
+        """
         return list(self._dict.keys())
